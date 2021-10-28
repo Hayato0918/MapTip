7c70e02d4dea78b1b19c9304cf80d488d343d46b
sugar <KH.Hayato0918@gmail.com>
2021-10-28 17:51:40 +0900

    X,Yのサイズをそれぞれ変更できるようにした

--- マップチップ作成ツール/player.cpp
+++ マップチップ作成ツール/player.cpp
@@ -26,7 +26,8 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define PIXEL_NUM 10
+#define PIXEL_X 10
+#define PIXEL_Y 10
 
 //*****************************************************************************
 // プロトタイプ宣言
@@ -37,9 +38,9 @@ bool Mouse_LClick;
 
 D3DXCOLOR color;
 
-PIXEL pixel[PIXEL_NUM][PIXEL_NUM];
-TIP tip[PIXEL_NUM][PIXEL_NUM];
-int check[PIXEL_NUM][PIXEL_NUM];
+PIXEL pixel[PIXEL_X][PIXEL_Y];
+TIP tip[PIXEL_X][PIXEL_Y];
+int check[PIXEL_X][PIXEL_Y];
 
 //*****************************************************************************
 // グローバル変数
@@ -56,15 +57,15 @@ HRESULT InitPlayer(void)
 	pixel_texture = LoadTexture("data/TEXTURE/pixel.png");
 	red_texture = LoadTexture("data/TEXTURE/red.png");
 
-	for (int i = 0; i < PIXEL_NUM; i++)
+	for (int i = 0; i < PIXEL_Y; i++)
 	{
-		for (int j = 0; j < PIXEL_NUM; j++)
+		for (int j = 0; j < PIXEL_X; j++)
 		{
-			pixel[j][i].pos = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_NUM * i, SCREEN_HEIGHT / PIXEL_NUM * j);
-			pixel[j][i].size = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_NUM, SCREEN_HEIGHT / PIXEL_NUM);
+			pixel[j][i].pos = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_X * i, SCREEN_HEIGHT / PIXEL_Y * j);
+			pixel[j][i].size = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_X, SCREEN_HEIGHT / PIXEL_Y);
 
-			tip[j][i].pos = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_NUM * i, SCREEN_HEIGHT / PIXEL_NUM * j);
-			tip[j][i].size = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_NUM, SCREEN_HEIGHT / PIXEL_NUM);
+			tip[j][i].pos = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_X * i, SCREEN_HEIGHT / PIXEL_Y * j);
+			tip[j][i].size = D3DXVECTOR2(SCREEN_WIDTH / PIXEL_X, SCREEN_HEIGHT / PIXEL_Y);
 			tip[j][i].drawflag = false;
 
 			check[j][i] = 0;
@@ -92,9 +93,9 @@ void UpdatePlayer(void)
 	Mouse_LClick = GetMouseLClick();
 
 	//マップお絵かき
-	for (int i = 0; i < PIXEL_NUM; i++)
+	for (int i = 0; i < PIXEL_Y; i++)
 	{
-		for (int j = 0; j < PIXEL_NUM; j++)
+		for (int j = 0; j < PIXEL_X; j++)
 		{
 			//if (Mouse_X > pixel[j][i].pos.x && Mouse_X < pixel[j + 1][i + 1].pos.x && Mouse_Y > pixel[j][i].pos.y && Mouse_Y < pixel[j + 1][i + 1].pos.y)
 			if (Mouse_X > pixel[j][i].pos.x && Mouse_X < pixel[j][i].pos.x + pixel[j][i].size.x &&
@@ -103,7 +104,6 @@ void UpdatePlayer(void)
 				if (Mouse_LClick == true)
 					tip[j][i].drawflag = true;
 			}
-
 
 			if (tip[j][i].drawflag == true)
 			{
@@ -117,22 +117,21 @@ void UpdatePlayer(void)
 	{
 		FILE* fp;
 		fp = fopen("マップチップデータ.txt", "w");
-		for (int i = 0; i < PIXEL_NUM; i++)
+		for (int i = 0; i < PIXEL_Y; i++)
 		{
 			fprintf(fp, "{");
-			for (int j = 0; j < PIXEL_NUM; j++)
+			for (int j = 0; j < PIXEL_X; j++)
 			{
 				fprintf(fp, "%d", check[i][j]);
-				if (j < PIXEL_NUM - 1)
+				if (j < PIXEL_X - 1)
 					fprintf(fp, ",");
 			}
 			fprintf(fp, "}");
-			if(i < PIXEL_NUM - 1)
+			if(i < PIXEL_Y - 1)
 			fprintf(fp, ",\n");
 		}
 		fclose(fp);
 	}
-
 }
 
 //=============================================================================
@@ -140,9 +139,9 @@ void UpdatePlayer(void)
 //=============================================================================
 void DrawPlayer(void)
 {
-	for (int i = 0; i < PIXEL_NUM; i++)
+	for (int i = 0; i < PIXEL_Y; i++)
 	{
-		for (int j = 0; j < PIXEL_NUM; j++)
+		for (int j = 0; j < PIXEL_X; j++)
 		{
 			DrawSpriteLeftTop(pixel_texture, pixel[j][i].pos.x, pixel[j][i].pos.y, pixel[j][i].size.x, pixel[j][i].size.y, 0.0f, 0.0f, 1.0f, 1.0f);
 			{
@@ -151,6 +150,4 @@ void DrawPlayer(void)
 			}
 		}
 	}
-
-	//DrawSpriteColor(pixel_texture, 80 * 2.5, 80 * 9.5, 80 * 3, 80, 0.0f, 0.0f, 1.0f, 1.0f, color);
 }
